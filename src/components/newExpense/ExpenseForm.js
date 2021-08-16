cf1ab6dde7ac32dfc4cac207ce9efa68076e4af5
Lucas <lucas.souza@ifood.com.br>
2021-08-16 14:24:06 -0300

    add cancel button on new expense component

--- src/components/newExpense/ExpenseForm.js
+++ src/components/newExpense/ExpenseForm.js
@@ -69,6 +69,10 @@ export default function ExpenseForm(props) {
       </div>
 
       <div className="new-expense__actions">
+        <button type="button" onClick={props.onCancel}>
+          Cancel
+        </button>
+
         <button type="submit">Add expense</button>
       </div>
     </form>
